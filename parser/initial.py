cabac8d9b81afea46f9cd88e3db57b4652849b57
mstojanovicm00 <mstojanovicm00@gmail.com>
2025-11-09 16:39:36 +0100

    Some routine changes

--- parser/initial.py
+++ parser/initial.py
@@ -1,6 +1,10 @@
 from lark import Lark
 from parser import grammar
 
+def write_tree(tree, filename):
+    with open(filename, "w") as f:
+        f.write(tree.pretty('\t'))
+
 def parse_to_first_tree(code):
     parser = Lark(grammar.grammar, start="program", parser="lalr", lexer="contextual")
     tree = None
@@ -8,5 +12,7 @@ def parse_to_first_tree(code):
         tree = parser.parse(code)
     finally:
         if tree:
-            print(tree.pretty(' '))
+            write_tree(tree, 'tree.txt')
+        else:
+            print("No tree was generated")
     return tree
